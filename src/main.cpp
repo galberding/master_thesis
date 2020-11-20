077b8456b529e624f8855f0bd7c0bf491b39e933
galberding <galberding@techfak.uni-bielefeld.de>
2020-11-20 17:11:01 +0000

    Start using grid_map

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,23 @@ using namespace std;
 
 using namespace cv;
 using namespace grid_map;
+#define w 400
 
+
+void MyEllipse( Mat img, double angle )
+{
+  int thickness = 2;
+  int lineType = 8;
+  ellipse( img,
+	   Point( 100, 100 ),
+       cv::Size( 10, 5 ),
+       angle,
+       90,
+       360,
+       Scalar(255),
+       thickness,
+       4 );
+}
 
 int main(int argc, char *argv[])
 {
@@ -47,16 +63,43 @@ int main(int argc, char *argv[])
 
   vector<Point> waypoints = opti_ga::genWaypoints(width, height, start, 10);
 
-  opti_ga::GenPool pool(width, height, start, stop, rob_size, rob_speed, offspringCount);
-  pool.populatePool(1000, 5);
+  // opti_ga::GenPool pool(width, height, start, stop, rob_size, rob_speed, offspringCount);
+  // pool.populatePool(1000, 5);
   // pool.update(10000);
-  grid_map::GridMap gmap({"color"});
+  grid_map::GridMap gmap;
   gmap.setFrameId("map");
-  gmap.setGeometry(Length(10,10), 0.5);
-  cout << gmap.getLength().x() << endl;
+  gmap.setGeometry(Length(width,height), 1);
+  cout << gmap.getSize()(0) << endl;
 
   // grid_map::GridMapRosConverter::initializeFromImage(const sensor_msgs::Image &image, const double resolution, grid_map::GridMap &gridMap)
-  grid_map::GridMapCvConverter::initializeFromImage(map3, 0.01, gmap, grid_map::Position(0,0));
+  // cout << "Img to map: " << grid_map::GridMapCvConverter::initializeFromImage(map3, 0.01, gmap, grid_map::Position(0,0)) << endl;
+  cout << "Img to map: " <<  GridMapCvConverter::addLayerFromImage<unsigned short, 1>(map3, "path", gmap) << endl;
+
+
+  grid_map::Index idx_start(18, 2);
+  grid_map::Index idx_end(2, 13);
+  for(grid_map::LineIterator it(gmap,idx_start, idx_end); !it.isPastEnd(); ++it){
+    cout << *it << endl;
+    gmap.at("path", *it) = 255;
+  }
+
+  // for(grid_map::EllipseIterator it())
+
+  // for(auto &lay : gmap.getLayers()){
+  //   cout << "Layer: " <<  lay << endl;
+  // }
+  cout << "Before" << "\n";
+  GridMapCvConverter::toImage<unsigned char, 1>(gmap, "path", CV_8U, 0, 255, map3);
+  cout << "After" << "\n";
+
+
+  MyEllipse(map3, 0);
+
+
+  imshow("Final Image", map3);
+  waitKey();
+
+
   // Point A(2,2), B(1,2);
 
   // cout << A.dot(B) / (norm(A)*norm(B)) << endl;
